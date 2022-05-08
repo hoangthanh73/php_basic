96f0c20328579b6641cfe2592dc2122c2f502c35
minhthanh73 <hoangminhthanh867@gmail.com>
2022-05-08 17:01:53 +0700

    while

--- bai13.php
+++ bai13.php
@@ -4,8 +4,9 @@
 $number = 10;
 $sumNumber = 0;
 
-while ($number > 0){
-    $sumNumber += $number; $number -= 3;//giảm 3 đơn vị sau mỗi lần lặp
+while ($number > 0) {
+    $sumNumber += $number;
+    $number -= 3; //giảm 3 đơn vị sau mỗi lần lặp
 }
 echo $sumNumber;
 echo '<br/>';
@@ -13,20 +14,20 @@ echo '<br/>';
 $i = 1;
 
 do {
-    echo $i; $i ++;
+    echo $i;
+    $i++;
 } while (
     $i < 1
 );
 echo '<br/>';
 //vòng lặp while lồng nhau
 $i = 1;
-while ($i < 10)
-{
+while ($i < 10) {
     $j = $i;
-    while ($j < 10)
-    {
-        echo $j; $j++;
+    while ($j < 10) {
+        echo $j;
+        $j++;
     }
-        echo '<br/>';
+    echo '<br/>';
     $i++;
 }
