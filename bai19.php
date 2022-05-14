0dc112841e027f9b8a0c9f18e29be65f6a1537f0
minhthanh73 <hoangminhthanh867@gmail.com>
2022-05-14 21:37:13 +0700

    tinhtongsochan

---
+++ bai19.php
@@ -0,0 +1,26 @@
+<?php
+
+// Viết chương trình tinhd tổng số nguyên chẵn từ 1 đến n
+
+$n = 100;
+$numberEven = [];
+$sum = null;
+for ($i = 1; $i <= $n; $i++) {
+    if ($i % 2 == 0) {
+        $numberEven[] = $i;
+        //$sum += $i;
+    }
+}
+//echo "tổng là $sum";
+//echo '<br/>';
+echo 'Số chẵn là: ';
+foreach ($numberEven as $key => $val) {
+    if ($n - $val <= 1) {
+        echo $val;
+    } else {
+        echo $val . '; ';
+    }
+    $sum += $val;
+}
+echo '<br/>';
+echo "Tổng là: $sum";
