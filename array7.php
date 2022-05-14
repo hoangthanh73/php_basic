e60bd80db42c0a28a242473b0c372bc04d86f591
minhthanh73 <hoangminhthanh867@gmail.com>
2022-05-14 21:37:49 +0700

    array

---
+++ array7.php
@@ -0,0 +1,35 @@
+<?php
+
+$listName = ['Thành' => 23, 'Công' => '24', 'Tiến' => 21, 'Mỹ' => 26];
+
+echo 'sắp xếp mảng theo value tăng dần asort() <br/>';
+
+asort($listName);
+foreach ($listName as $name => $age) {
+    echo "$name: $age tuổi";
+    echo '<br/>';
+}
+
+echo 'sắp xếp mảng  theo value giảm dần arsort() <br/>';
+
+arsort($listName);
+foreach ($listName as $name => $age) {
+    echo "$name: $age tuổi";
+    echo '<br/>';
+}
+
+echo 'sắp xếp theo key tăng dần ksort() <br/>';
+
+ksort($listName);
+foreach ($listName as $name => $age) {
+    echo "$name: $age tuổi";
+    echo '<br/>';
+}
+
+echo 'sắp xếp theo key giảm dần krsort() <br/>';
+
+krsort($listName);
+foreach ($listName as $name => $age) {
+    echo "$name: $age tuổi";
+    echo '<br/>';
+}
