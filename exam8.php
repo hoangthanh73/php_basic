bf8d3ab48e927dcaa1fc094b880dacbc7492cc5e
minhthanh73 <hoangminhthanh867@gmail.com>
2022-05-16 10:11:40 +0700

    array

--- exam8.php
+++ exam8.php
@@ -1,2 +1,15 @@
 <?php
-echo ' hoang thanh';
+// viết hàm gán giá trị mảng
+
+function tao_mang_so_nguyen($n)
+{
+    for ($i = 0; $i <= $n; $i++) {
+        echo $number[] = $i;
+    }
+    return;
+}
+$number[] = (tao_mang_so_nguyen(10));
+
+foreach ($number as $val) {
+    echo $val;
+}
